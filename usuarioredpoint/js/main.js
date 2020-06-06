14af4bec3d262791818000f19e0bb1eee7d9dd10
EdTorres <l.torres@supaada.mx>
2020-06-05 22:12:24 -0500

    Vistas RP Web 2020

---
+++ usuarioredpoint/js/main.js
@@ -0,0 +1,23 @@
+
+var botonenvio = document.getElementById("formulario_envio");
+console.log(botonenvio);
+
+botonenvio.addEventListener("click",error_date);
+
+/*function error_date(){
+
+    //var entradaName = document.getElementById("name").value;
+    var entradaLastName = document.getElementById("lastname").value;
+    var entradaEmail = document.getElementById("email").value;
+    var entradaPassword = document.getElementById("password").value;
+
+if(entradaLastName === "" || entradaEmail === "" || entradaPassword === "" ){
+
+    alert("El campo esta vacio");
+
+    
+}
+
+
+}*/
+
